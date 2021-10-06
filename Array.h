f8f4edb48a7fe1f22dbd17f2ca50c7d8da113f66
Koribu <snnklp@gmail.com>
2021-10-06 11:39:10 -0400

    Add project files.

---
+++ Array.h
@@ -0,0 +1,113 @@
+#pragma once
+#include <assert.h>
+
+template<class T>
+class Array
+{
+public:
+
+	Array(int size, int growBy = 1)
+	{
+
+	}
+	~Array();
+	T& operator[](int index)
+	{
+		assert(m_array != nullptr && index < m_numElements);
+		return m_array[index];
+
+	}
+
+	virtual void push(T val);
+	void pop()
+	{
+		if (m_numElements > 0)
+		{
+			m_numElements--;	// Reduce the total number of elements by 1. Ignoring the last element.
+		}
+	}
+
+	void remove(int index)
+	{
+		assert(m_array != nullptr);
+
+		if (index >= m_numElements)
+		{
+			// I am trying to remove something outside of the bounds of the array
+			return;	// <-- Maybe could do some form of exception handling
+		}
+
+		for (int i = index; i < m_numElements; i++)
+		{
+			// Start at the index we want to remove.
+			// Shift everything after index back by one.
+			if (i + 1 < m_numElements)	// Confines the loop into the array
+			{
+				m_array[i] = m_array[i + 1];
+			}
+		}
+		m_numElements--;
+	}
+
+	virtual int search(T val)
+	// Clear
+	void clear()
+	{
+		m_numElements = 0;	 // Ignore (or forgets) all current items in the array
+	}
+	// Gets and Sets
+	int GetSize()
+	{
+		return m_numElements;
+	}
+	int GetMaxSize()
+	{
+		return m_maxSize;
+	}
+	int GetGrowSize()
+	{
+		return m_growSize;
+	}
+	int SetGrowSize(int val)
+	{
+		assert(val >= 0);
+		m_growSize = val;
+	}
+private:
+	// Private functions
+		// Expansion
+	bool Expand()
+	{
+		if (m_growSize <= 0)
+		{
+			// LEAVE!
+			return false;
+		}
+
+		// Create the new array
+		T* temp = new T[m_maxSize + m_growSize];
+		assert(temp != nullptr);
+
+		// Copy the contents of the original array into the new array
+		memcpy(temp, m_array, sizeof(T) * m_maxSize);
+
+		// Delete the old array
+		delete[] m_array;
+
+		// Clean up variable assignments
+		m_array = temp;
+		temp = nullptr;
+
+		m_maxSize += m_growSize;
+
+		return true;
+	}
+private:
+	T* m_array;			// Pointer to the beginning of the array
+
+	int m_maxSize;		// Maximum size of the array
+	int m_growSize;		// Amount the array can grow through expansion
+	int m_numElements;	// Number of items currently in my array
+
+};
+
